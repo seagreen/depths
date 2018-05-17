c470498267a14fb629d7f1dbe92ee237a979de33
Ian Grant Jeffries <ian@housejeffries.com>
2018-05-17 19:53:44 -0400

    Fill out tech table.

--- src/View/TechTable.elm
+++ src/View/TechTable.elm
@@ -3,37 +3,143 @@ module View.TechTable exposing (..)
 import Game.Building as Building exposing (Building)
 import Game.Unit as Unit exposing (Submarine)
 import Html exposing (Html)
+import Html.Attributes as Hattr
 
 
 view : Html msg
 view =
-    Html.div [] <|
-        (List.map viewSubmarine Unit.all ++ List.map viewBuilding Building.all)
+    Html.div
+        []
+        [ Html.h2
+            []
+            [ Html.text "Submarines" ]
+        , Html.table
+            [ Hattr.class "table table-bordered table-striped"
+            ]
+            [ Html.thead []
+                [ Html.tr
+                    []
+                    [ Html.th
+                        []
+                        [ Html.text "Name" ]
+                    , Html.th
+                        []
+                        [ Html.text "Sensors" ]
+                    , Html.th
+                        []
+                        [ Html.text "Firepower" ]
+                    , Html.th
+                        []
+                        [ Html.text "Stealth" ]
+                    , Html.th
+                        []
+                        [ Html.text "Cost" ]
+                    , Html.th
+                        []
+                        [ Html.text "Prerequisites" ]
+                    ]
+                ]
+            , Html.tbody
+                []
+                (List.map viewSubmarine Unit.all)
+            ]
+        , Html.h2
+            []
+            [ Html.text "Buildings" ]
+        , Html.table
+            [ Hattr.class "table table-bordered table-striped"
+            ]
+            [ Html.thead []
+                [ Html.tr
+                    []
+                    [ Html.th
+                        []
+                        [ Html.text "Name" ]
+                    , Html.th
+                        []
+                        [ Html.text "Sensors" ]
+                    , Html.th
+                        []
+                        [ Html.text "Firepower" ]
+                    , Html.th
+                        []
+                        [ Html.text "Production bonus" ]
+                    , Html.th
+                        []
+                        [ Html.text "Cost" ]
+                    , Html.th
+                        []
+                        [ Html.text "Prerequisites" ]
+                    ]
+                ]
+            , Html.tbody
+                []
+                (List.map viewBuilding Building.all)
+            ]
+        ]
 
 
 viewSubmarine : Submarine -> Html msg
 viewSubmarine submarine =
-    case Unit.stats submarine of
-        { name, prerequisites } ->
-            Html.div []
-                [ Html.text
-                    (name
-                        ++ " <-- "
-                        ++ String.join ", "
-                            (List.map toString prerequisites)
-                    )
-                ]
+    let
+        stats =
+            Unit.stats submarine
+    in
+    Html.tr
+        []
+        [ Html.td
+            []
+            [ Html.text stats.name ]
+        , Html.td
+            []
+            [ Html.text (toString stats.sensors) ]
+        , Html.td
+            []
+            [ Html.text (toString stats.firepower) ]
+        , Html.td
+            []
+            [ Html.text (toString stats.stealth) ]
+        , Html.td
+            []
+            [ Html.text (toString stats.cost) ]
+        , Html.td
+            []
+            [ Html.text (String.join ", " (List.map toString stats.prerequisites)) ]
+        ]
 
 
 viewBuilding : Building -> Html msg
 viewBuilding building =
-    case Building.stats building of
-        { name, prerequisites } ->
-            Html.div []
-                [ Html.text
-                    (name
-                        ++ " <-- "
-                        ++ String.join ", "
-                            (List.map toString prerequisites)
-                    )
-                ]
+    let
+        stats =
+            Building.stats building
+
+        ( sensors, firepower ) =
+            case stats.combatStats of
+                Just { sensors, firepower } ->
+                    ( toString sensors, toString firepower )
+
+                Nothing ->
+                    ( "", "" )
+    in
+    Html.tr
+        []
+        [ Html.td
+            []
+            [ Html.text stats.name ]
+        , Html.td
+            []
+            [ Html.text sensors ]
+        , Html.td
+            []
+            [ Html.text firepower ]
+        , Html.td
+            []
+            [ Html.text (toString stats.productionBonus) ]
+        , Html.td
+            []
+            [ Html.text (toString stats.cost) ]
+        , Html.td
+            []
+            [ Html.text (String.join ", " (List.map toString stats.prerequisites)) ]
+        ]
